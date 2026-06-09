92e6a05c708d117659ff5e06b2472bfaccd044e5
Rebaone Seilane <rebaoneseilane@gmail.com>
2026-06-09 12:07:20 +0200

    Task3

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - Checks if a node is root
+ * @node: Pointer to node
+ *
+ * Return: 1 if root, 0 otherwise
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->parent == NULL)
+		return (1);
+
+	return (0);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -26,6 +26,21 @@ void binary_tree_delete(binary_tree_t *tree);
 /* Task 4 */
 int binary_tree_is_leaf(const binary_tree_t *node);
 
+/* Task 5 */
+int binary_tree_is_root(const binary_tree_t *node);
+
+/* Task 6 */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 7 */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 8 */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 9 */
+size_t binary_tree_height(const binary_tree_t *tree);
+
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
