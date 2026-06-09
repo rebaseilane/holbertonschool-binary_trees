757bb9c386d35db0b9a8d594fc13cef75ca4f71b
Rebaone Seilane <rebaoneseilane@gmail.com>
2026-06-09 15:12:07 +0200

    Task4

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_nodes - Counts nodes with at least one child
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Number of non-leaf nodes, or 0 if tree is NULL
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
